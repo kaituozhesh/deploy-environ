92dcf810992921a79353f088fe52c1402f2c0a03
kaituozhesh <kaituozhesh@hotmail.com>
2020-03-08 10:46:04 +0800

    Druid 环境配置

---
+++ druid-environment/src/main/java/com/ktz/deploy/druid/DruidEnvironmentApplication.java
@@ -0,0 +1,18 @@
+package com.ktz.deploy.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description:
+ *
+ * @author kaituozhe_sh
+ */
+@SpringBootApplication
+public class DruidEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidEnvironmentApplication.class, args);
+    }
+
+}
---
+++ druid-environment/src/main/java/com/ktz/deploy/druid/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.ktz.deploy.druid.mapper;
+
+/**
+ * Description:
+ * date: 2020/3/8 10:45
+ *
+ * @author kaituozhe_sh
+ */
+public interface UserMapper {
+}
